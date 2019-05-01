e347e7640567d7c0f296b17ecb53f9ae72374123
fuliang <fuliang@jk724.com>
2019-05-01 21:20:49 +0800

    添加基于abp开发基础code

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,6 +7,8 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
+  headers: { 'Content-Type': 'application/json' },
+  responseType: 'json', // 服务器返回的数据类型
   timeout: 5000 // request timeout
 })
 
@@ -34,7 +36,7 @@ service.interceptors.response.use(
   /**
    * If you want to get information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
@@ -43,32 +45,35 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(res.message || 'error')
-    } else {
-      return res
-    }
+    return res
+    // // if the custom code is not 20000, it is judged as an error.
+    // if (res.code !== 20000) {
+    //   Message({
+    //     message: res.message || 'error',
+    //     type: 'error',
+    //     duration: 5 * 1000
+    //   })
+    //   // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+    //   if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+    //     // to re-login
+    //     MessageBox.confirm(
+    //       'You have been logged out, you can cancel to stay on this page, or log in again',
+    //       'Confirm logout',
+    //       {
+    //         confirmButtonText: 'Re-Login',
+    //         cancelButtonText: 'Cancel',
+    //         type: 'warning'
+    //       }
+    //     ).then(() => {
+    //       store.dispatch('user/resetToken').then(() => {
+    //         location.reload()
+    //       })
+    //     })
+    //   }
+    //   return Promise.reject(res.message || 'error')
+    // } else {
+    //   return res
+    // }
   },
   error => {
     console.log('err' + error) // for debug
